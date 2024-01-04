e03fa779baaa25b92541a123637ce47f412bd75e
Ramadhani <ramadhanifals@gmail.com>
2024-01-04 06:08:50 +0000

    Bringup AwakenOS

--- BoardConfigAwaken.mk
+++ BoardConfigAwaken.mk
@@ -29,8 +29,8 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
-DEVICE_MANIFEST_FILE += device/google/redbull/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_MANIFEST_FILE += device/google/redbull/awaken_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/awaken/config/device_framework_matrix.xml
 
 # Needed for camera
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
@@ -45,8 +45,8 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 -include vendor/lineage/config/BoardConfigReservedSize.mk
 
 # SELinux
-BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-lineage/dynamic
-BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-lineage/vendor
+BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-awaken/dynamic
+BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-awaken/vendor
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
